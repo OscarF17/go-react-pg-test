fcbf2f7ba0dcef04d675b655af5a3385898032d9
Oscar Flores <oscar.floresl@udem.edu>
2024-06-09 12:29:15 -0600

    add db healthcheck on compose file, remove backend delay

--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,11 @@ import (
 	"backend/config"
 	"backend/routes"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func main() {
 	// Wait for database to be ready
 	// Added to keep container from crashing when running for the first time
-	time.Sleep(5 * time.Second)
 	router := gin.New()
 	config.Conncect()
 	routes.DefRoutes(router)
